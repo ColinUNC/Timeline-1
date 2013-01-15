6a4694f1e3f8087fe2c7165423cb25480e647ca7
Mihael Isaev <isaev.mihael@gmail.com>
2013-01-15 18:07:08 +0400

    Initial Commit

---
+++ Timeline/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Timeline
+//
+//  Created by mihael on 15.01.13.
+//  Copyright (c) 2013 Mihael Isaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Timeline/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Timeline
+//
+//  Created by mihael on 15.01.13.
+//  Copyright (c) 2013 Mihael Isaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
